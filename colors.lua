4f62cf56f98791c8756ccc9d70adb1cedc575cae
Divij Ganjoo <divijganjoo2003@gmail.com>
2025-01-23 01:18:42 +0530

    Update colors.lua
    Updated rarity interface display

--- colors.lua
+++ colors.lua
@@ -1,26 +1,29 @@
 -- colors.lua
-local colors = {
-    -- ANSI color codes
-    rarityColors = {
-        Common = "\27[90m",     -- Grey
-        Uncommon = "\27[92m",   -- Green
-        Epic = "\27[94m",       -- Blue
-        Elite = "\27[95m",      -- Purple
-        Unique = "\27[93m",     -- Gold
-        Legendary = "\27[91m",  -- Red
-    },
-    reset = "\27[0m"
+
+local colors = {}
+
+-- ANSI color codes for rarities
+colors.rarityColors = {
+    Common = "\27[90m",     -- Grey
+    Uncommon = "\27[92m",   -- Green
+    Epic = "\27[94m",       -- Blue
+    Elite = "\27[95m",      -- Purple
+    Unique = "\27[93m",     -- Gold
+    Legendary = "\27[91m",  -- Red
 }
 
--- Function to color text based on rarity
+-- Reset ANSI color
+colors.reset = "\27[0m"
+
+-- Function to colorize text based on rarity
 function colors.colorize(text, rarity)
     local color = colors.rarityColors[rarity] or colors.reset
     return color .. text .. colors.reset
 end
 
--- Function to color unit name and class
-function colors.formatUnitName(rarity, class)
-    return colors.colorize(rarity .. " " .. class, rarity)
+-- Function to format unit name with rarity-based color
+function colors.formatUnitName(rarity, name)
+    return colors.colorize(name, rarity)
 end
 
 return colors
